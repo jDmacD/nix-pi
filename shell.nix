49d5acb4ca53d1f9e4040b3efaa0f2b4a843096d
jDmacD <jmacdoo@gmail.com>
2026-07-20 22:49:27 +0100

    feat(shell): add ci build and push

--- shell.nix
+++ shell.nix
@@ -1,22 +1,43 @@
 {
   perSystem =
     { pkgs, inputs', ... }:
+    let
+      build-push = pkgs.writeShellApplication {
+        name = "build-push";
+        runtimeInputs = [
+          pkgs.cachix
+          pkgs.omnix
+        ];
+        text = ''
+          nix flake update --accept-flake-config
+          om ci run .# \
+          --systems=aarch64-linux \
+          --include-all-dependencies -- --accept-flake-config \
+            | xargs cachix push jdmacd
+        '';
+      };
+    in
     {
       devShells.default = pkgs.mkShell {
         name = "nix-pi";
-        packages = with pkgs; [
-          # fleet deployment (see deploy.nix)
-          inputs'.deploy-rs.packages.default
-          # secrets management (see modules/nixos/sops.nix, .sops.yaml)
-          sops
-          age
-          ssh-to-age
-          # disk layout for Pi 5 / CM4 hosts
-          disko
-          # nix tooling
-          nixfmt
-          nix-tree
-        ];
+        packages =
+          with pkgs;
+          [
+            # fleet deployment (see deploy.nix)
+            inputs'.deploy-rs.packages.default
+            # secrets management (see modules/nixos/sops.nix, .sops.yaml)
+            sops
+            age
+            ssh-to-age
+            # disk layout for Pi 5 / CM4 hosts
+            disko
+            # nix tooling
+            nixfmt
+            nix-tree
+            omnix
+            cachix
+          ]
+          ++ ([ build-push ]);
       };
     };
 }
